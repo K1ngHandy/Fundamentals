f2fa0280f8e237a743dc94ae52614c659b0dda81
K1ngHandy <bolds-slicks0g@icloud.com>
2024-08-10 17:26:37 -0400

    add file

---
+++ Fundamentals.swift
@@ -0,0 +1,18 @@
+//
+//  Fundamentals.swift
+//  
+//
+//  Created by Steve Handy on 2024.08.10.
+//
+
+import SwiftUI
+
+struct Fundamentals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Fundamentals()
+}
