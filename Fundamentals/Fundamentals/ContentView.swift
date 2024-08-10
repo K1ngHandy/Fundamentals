7d5e6876bd09f573ec24c0697caf897eb710352a
K1ngHandy <bolds-slicks0g@icloud.com>
2024-08-10 17:35:34 -0400

    convert to xcode file

---
+++ Fundamentals/Fundamentals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fundamentals
+//
+//  Created by Steve Handy on 2024.08.10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fundamentals/Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Steve Handy on 2024.08.10.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
